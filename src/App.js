6bb339bd20d570fd91f7f250b798146ee70781d4
Geletsky <nsurcov98@gmail.com>
2023-06-05 11:51:52 +0300

    Added PostItem component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
+import './styles/App.css'
+import PostItem from "./components/PostItem";
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+	const [posts, setPosts] = useState([
+		{ id: 1, title: 'Title 1', body: 'Description 1' },
+		{ id: 2, title: 'Title 2', body: 'Description 2' },
+		{ id: 3, title: 'Title 3', body: 'Description 3' },
+	])
+
+	return (
+		<div className="App">
+			{posts.map(post => <PostItem post={post} key={post.id} />)}
+		</div>
+	);
 }
 
 export default App;
