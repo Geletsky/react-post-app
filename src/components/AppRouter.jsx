d517c53215e72583aa73b3e70f78204156b82ab8
Geletsky <nsurcov98@gmail.com>
2023-06-07 21:23:59 +0300

    Optimized navigation realization

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
-import { Routes, Route } from "react-router-dom";
-import About from "../pages/About";
 import Posts from "../pages/Posts";
-import Error from "../pages/Error";
-import PostIdPage from '../pages/PostIdPage';
+import { Routes, Route } from "react-router-dom";
+import { routes } from '../router/routes';
+
 export default function AppRouter() {
-  return (
-	  <Routes>
-		  <Route element={<About />} path="/about" />
-		  <Route element={<PostIdPage />} exact path="/posts/:id" />
-		  <Route element={<Posts />} exact path="/posts" />
-		  <Route element={<Error />} path="*" />
-	  </Routes>
-  )
+	return (
+		<Routes>
+			{routes.map(route => 
+				<Route key={route.path} element={route.component} path={route.path} exact={route.exact} />
+			)}
+			<Route element={<Posts />} path="*" />
+		</Routes>
+	)
 }
--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -7,7 +7,7 @@ export default function PostItem(props) {
 	return (
 		<div className="post">
 			<div className="post__content">
-				<h2 className="post__title">{props.post.id}. {props.post.title}</h2>
+				<h2 className="post__title">{props.number}. {props.post.title}</h2>
 				<div className="post__text">{props.post.body}</div>
 			</div>
 			<div className="post__buttons">
