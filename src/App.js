5efb3c9245c6f3a2c1677d6cdaa7b94a957b29af
Geletsky <nsurcov98@gmail.com>
2023-06-07 13:16:13 +0300

    Extracted filtering and sorting logic into a separate hook 'usePosts'

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React, { useState, useMemo } from "react";
+import React, { useState } from "react";
 import './components/styles/App.css';
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
 import PostFilter from "./components/PostFilter";
 import MyModal from "./components/UI/MyModal/MyModal";
 import MyButton from "./components/UI/button/MyButton";
+import { usePosts } from "./hooks/usePosts";
 
 function App() {
 	const [posts, setPosts] = useState([
@@ -15,17 +16,7 @@ function App() {
 
 	const [filter, setFilter] = useState({ sort: '', query: '' });
 	const [modal, setModal] = useState(false);
-
-	const sortedPosts = useMemo(() => {
-		if (filter.sort) {
-			return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]))
-		}
-		return posts
-	}, [filter.sort, posts]);
-
-	const sortedAndSearchedPosts = useMemo(() => {
-		return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query.toLowerCase()))
-	}, [filter.query, sortedPosts])
+	const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
 
 	function createPost(newPost) {
 		setPosts([...posts, newPost]);
