280c39be0ccb6dfd457d8ce907b9a7f20d8de73b
Geletsky <nsurcov98@gmail.com>
2023-06-07 20:21:40 +0300

    Added PostIdPage component for a detailed description of the post

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -3,11 +3,13 @@ import { Routes, Route } from "react-router-dom";
 import About from "../pages/About";
 import Posts from "../pages/Posts";
 import Error from "../pages/Error";
+import PostIdPage from '../pages/PostIdPage';
 export default function AppRouter() {
   return (
 	  <Routes>
 		  <Route element={<About />} path="/about" />
-		  <Route element={<Posts />} path="/posts" />
+		  <Route element={<PostIdPage />} exact path="/posts/:id" />
+		  <Route element={<Posts />} exact path="/posts" />
 		  <Route element={<Error />} path="*" />
 	  </Routes>
   )
--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import MyButton from './UI/button/MyButton'
+import { useNavigate } from 'react-router-dom'
 
 export default function PostItem(props) {
-  return (
-	  <div className="post">
-		  <div className="post__content">
-			  <h2 className="post__title">{props.post.id}. {props.post.title}</h2>
-			  <div className="post__text">{props.post.body}</div>
-		  </div>
-		  <div className="post__buttons">
-			  <MyButton onClick={() => props.remove(props.post)} className="post__button">Delete</MyButton>
-		  </div>
-	  </div>
-  )
+	const router = useNavigate();
+	return (
+		<div className="post">
+			<div className="post__content">
+				<h2 className="post__title">{props.post.id}. {props.post.title}</h2>
+				<div className="post__text">{props.post.body}</div>
+			</div>
+			<div className="post__buttons">
+				<MyButton onClick={() => router(`/posts/${props.post.id}`)} className="post__button">Open</MyButton>
+				<MyButton onClick={() => props.remove(props.post)} className="post__button">Delete</MyButton>
+			</div>
+		</div>
+	)
 }
