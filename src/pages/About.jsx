3c748bec35b42be3ef202adf15b5ab6766751564
Geletsky <nsurcov98@gmail.com>
2023-06-09 20:22:19 +0300

    Update About component
    - сhanged css styles

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,6 +2,33 @@ import React from 'react'
 
 export default function About() {
 	return (
-		<h1>About</h1>
+		<div className='about__container'>
+			<h1 style={{textAlign: 'center', marginTop: '20px'}}>About</h1>
+			<div style={{ marginTop: '20px' }}>
+				Lorem ipsum dolor sit amet 	Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi a fuga quasi reiciendis maxime, necessitatibus dicta maiores quia dolore autem repellendus tenetur quos, aliquid eveniet. Veritatis dolores nam iusto neque!
+				consectetur adipisicing elit. Modi a fuga	Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi a fuga quasi reiciendis maxime, necessitatibus dicta maiores quia dolore autem repellendus tenetur quos, aliquid eveniet. Veritatis dolores nam iusto neque!
+				quasi reiciendis maxime, necessitatibus dicta maiores quia dolore autem repellendus tenetur quos, aliquid eveniet. Veritatis dolores nam iusto neque!
+				<br /><br />
+				Lorem ipsum dolor sit amet 	Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi a fuga quasi reiciendis maxime, necessitatibus dicta maiores quia dolore autem repellendus tenetur quos, aliquid eveniet. Veritatis dolores nam iusto neque!
+				consectetur adipisicing elit. Modi a fuga	Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi a fuga quasi reiciendis maxime, necessitatibus dicta maiores quia dolore autem repellendus tenetur quos, aliquid eveniet. Veritatis dolores nam iusto neque!
+				quasi reiciendis maxime, necessitatibus dicta maiores quia dolore autem repellendus tenetur quos, aliquid eveniet. Veritatis dolores nam iusto neque!
+				<br /><br />
+				Lorem ipsum dolor sit amet 	Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi a fuga quasi reiciendis maxime, necessitatibus dicta maiores quia dolore autem repellendus tenetur quos, aliquid eveniet. Veritatis dolores nam iusto neque!
+				consectetur adipisicing elit. Modi a fuga 	Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi a fuga quasi reiciendis maxime, necessitatibus dicta maiores quia dolore autem repellendus tenetur quos, aliquid eveniet. Veritatis dolores nam iusto neque!
+				quasi reiciendis maxime, necessitatibus dicta maiores quia dolore autem repellendus tenetur quos, aliquid eveniet. Veritatis dolores nam iusto neque!
+				<br /><br />
+				Lorem ipsum dolor sit amet 	Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi a fuga quasi reiciendis maxime, necessitatibus dicta maiores quia dolore autem repellendus tenetur quos, aliquid eveniet. Veritatis dolores nam iusto neque!
+				consectetur adipisicing elit. Modi a fuga 	Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi a fuga quasi reiciendis maxime, necessitatibus dicta maiores quia dolore autem repellendus tenetur quos, aliquid eveniet. Veritatis dolores nam iusto neque!
+				quasi reiciendis maxime, necessitatibus dicta maiores quia dolore autem repellendus tenetur quos, aliquid eveniet. Veritatis dolores nam iusto neque!
+				<br /><br />
+				Lorem ipsum dolor sit amet 	Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi a fuga quasi reiciendis maxime, necessitatibus dicta maiores quia dolore autem repellendus tenetur quos, aliquid eveniet. Veritatis dolores nam iusto neque!
+				consectetur adipisicing elit. Modi a fuga 	Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi a fuga quasi reiciendis maxime, necessitatibus dicta maiores quia dolore autem repellendus tenetur quos, aliquid eveniet. Veritatis dolores nam iusto neque!
+				quasi reiciendis maxime, necessitatibus dicta maiores quia dolore autem repellendus tenetur quos, aliquid eveniet. Veritatis dolores nam iusto neque!
+				<br /><br />
+				Lorem ipsum dolor sit amet 	Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi a fuga quasi reiciendis maxime, necessitatibus dicta maiores quia dolore autem repellendus tenetur quos, aliquid eveniet. Veritatis dolores nam iusto neque!
+				consectetur adipisicing elit. Modi a fuga 	Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi a fuga quasi reiciendis maxime, necessitatibus dicta maiores quia dolore autem repellendus tenetur quos, aliquid eveniet. Veritatis dolores nam iusto neque!
+				quasi reiciendis maxime, necessitatibus dicta maiores quia dolore autem repellendus tenetur quos, aliquid eveniet. Veritatis dolores nam iusto neque!
+			</div>
+		</div>
 	)
 }
